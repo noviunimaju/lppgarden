87a4cbed1d33b3b6a586d024c6a835d1e2b637eb
noviunimaju <93240313+noviunimaju@users.noreply.github.com>
2021-10-27 11:40:05 +0700

    Add files via upload

---
+++ assets/js/function.js
@@ -0,0 +1,58 @@
+/* Alert*/
+let cookie;
+cookie = "yudi 20211027"
+/* Loop */
+// for (let i = 0;i<=10;i++){
+// console
+
+// while 
+/* Function */
+function greeting() {
+	if (nama_lengkap) {
+		console.log("user aman, bisa masuk web.");
+	} else {
+		console.log("user tidak aman, jangan kasih masuk");
+	}
+}
+greeting();
+
+function login(username, password) {
+if (username&& password) {
+	console.log (username + " dengan ");
+console.log("Password: " + password);
+} else if (username ||password) {
+console.log("username atau password salah.");
+} else {
+console.log ("Anda belum login");
+
+let uname = "Fulan";
+let pass = "1234";
+
+let haha = "haha";
+let hihi = "hihi";
+
+login(username, password);
+
+
+function luasSegitiga(a, tinggi) {
+let luas = alas * tinggi) / 2;
+return luas;
+
+}
+let alas = 10;
+let tinggi = 5;
+let luas = luasSegitiga(alas, tinggi);
+console.log(luas);
+
+
+/* Struktur Data */
+let dataMhs = ["Yudi", "Yuda", "Mia"};
+
+let biodataMhs = {
+	nama: "Yudi",
+	tempat_lahir: "Bandar Lampung",
+	negara: "Indonesia",
+},
+nim: "12",
+biodata: {
+	nama: "Yudi"
